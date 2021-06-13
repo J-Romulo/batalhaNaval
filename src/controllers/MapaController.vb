3358e26588c2205afb89dff2e88c27da771c9d3b
José Rômulo <joseromulo.10@hotmail.com>
2021-06-13 20:53:24 -0300

    Mapa controller

---
+++ src/controllers/MapaController.vb
@@ -0,0 +1,17 @@
+﻿Public Module MapaController
+    Public Function criarMapa(largura As Integer, altura As Integer)
+        Dim mapaCriado As New Mapa(altura, largura)
+
+        Return mapaCriado
+    End Function
+
+    Public Function setarJogada(mapa As Mapa, posicaoX As Integer, posicaoY As Integer)
+        If mapa.Item(posicaoX, posicaoY) Then
+            '''se ja tiver marcado a posição no array, significa que ja houve jogada nessa posicao
+            Return False
+        Else
+            mapa.Item(posicaoX, posicaoY) = 1
+            Return True
+        End If
+    End Function
+End Module
