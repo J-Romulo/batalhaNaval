7f0471158efe77aed7b734850f6a398bd5db6919
José Rômulo <joseromulo.10@hotmail.com>
2021-06-13 20:54:20 -0300

    Pequenas correções

--- src/models/Jogador.vb
+++ src/models/Jogador.vb
@@ -3,7 +3,7 @@ Imports System
 
 Public Class Jogador
     Private nome As String
-    Private tabuleiro As Tabuleiro
+    Private tabuleiro As Mapa
 
     Public Sub New(n As String)
         nome = n
@@ -13,11 +13,11 @@ Public Class Jogador
         Return nome
     End Function
 
-    Public Function getTabuleiro()
+    Public Function getMapa()
         Return tabuleiro
     End Function
 
-    Public Sub setTabuleiro(t As Tabuleiro)
+    Public Sub setTabuleiro(t As Mapa)
         tabuleiro = t
     End Sub
 
--- src/models/Mapa.vb
+++ src/models/Mapa.vb
@@ -1,23 +1,23 @@
 Public Class Mapa
-  Private Altura As Integer
-  Private Largura As Integer
-  Private TabuleiroMapa(,) As Integer
+    Private Altura As Integer
+    Private Largura As Integer
+    Private TabuleiroMapa(,) As Integer
 
-  Public Sub New(altura As Integer, largura As Integer)
-    Me.Altura = altura
-    Me.Largura = largura
-    Tabuleiro = new Integer(altura, largura) {}
-  End Sub
+    Public Sub New(altura As Integer, largura As Integer)
+        Me.Altura = altura
+        Me.Largura = largura
+        TabuleiroMapa = New Integer(altura, largura) {}
+    End Sub
 
-  ''' acessa posicao
-  Default Public Property Item(posicaoX As Integer, posicaoY As Integer) As Integer
-    Get 
-      Return Tabuleiro(posicaoX, posicaoY)
-    End Get
+    ''' acessa posicao
+    Default Public Property Item(posicaoX As Integer, posicaoY As Integer) As Integer
+        Get
+            Return TabuleiroMapa(posicaoX, posicaoY)
+        End Get
 
-    Set(valor As Integer)
-      Tabuleiro(posicaoX, posicaoY) = valor
-    End Set
-  End Property
+        Set(valor As Integer)
+            TabuleiroMapa(posicaoX, posicaoY) = valor
+        End Set
+    End Property
 
 End Class
