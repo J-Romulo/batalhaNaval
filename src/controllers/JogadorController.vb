1c5f389811a8f6623995e6f7013c29ef2acae87c
José Rômulo <joseromulo.10@hotmail.com>
2021-06-20 19:37:24 -0300

    Jogador Controller

---
+++ src/controllers/JogadorController.vb
@@ -0,0 +1,13 @@
+﻿Public Module JogadorController
+    Public Function criarBot(mapaJogador As Mapa)
+        Dim bot = New Jogador("Bot")
+        Dim tabuleiroBot = New Mapa(mapaJogador.getAltura, mapaJogador.getLargura, mapaJogador.getNaviosLength)
+        bot.setTabuleiro(tabuleiroBot)
+
+        Return bot
+    End Function
+
+    Public Function setarNaviosBot(bot As Jogador)
+
+    End Function
+End Module
