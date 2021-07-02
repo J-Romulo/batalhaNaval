e840793189cb287871d5682342878eb699939c40
José Rômulo <joseromulo.10@hotmail.com>
2021-07-02 18:50:21 -0300

    Adicionando Jogada de ataque do jogador

--- src/controllers/GameController.vb
+++ src/controllers/GameController.vb
@@ -17,7 +17,7 @@
         Dim bot As Jogador = JogadorController.criarBot(jogador.getMapa())
 
 
-        For j = 0 To mapa.getNaviosLength - 1
+        For j = 0 To mapa.getNaviosLength
             Dim navioEscolha = GameView.escolhaNavios() - 1
             Dim embarcacao = New Navio(navioEscolha)
 
@@ -27,8 +27,8 @@
                 GameView.setarPosicaoNavio(linha, coluna)
                 embarcacao.setPosicao(linha, coluna, i)
 
-                MapaController.setarJogada(jogador.getMapa, coluna, linha, embarcacao.getTipo)
-                MapaController.setarJogada(bot.getMapa, coluna, linha, embarcacao.getTipo)
+                MapaController.setarJogada(jogador.getMapa, linha, coluna, embarcacao.getTipo)
+                MapaController.setarJogada(bot.getMapa, linha, coluna, embarcacao.getTipo)
             Next
 
             MapaController.setarEmbarcacao(jogador.getMapa, embarcacao)
@@ -37,7 +37,24 @@
             GameView.imprimirTabuleiro(jogador.getMapa())
         Next
 
+        Dim resultadoRodada
+        While resultadoRodada <> 2
+            GameView.imprimirTabuleiroInimigo(bot.getMapa)
 
-        Console.WriteLine("{0}")
+            Dim linhaAtaque, colunaAtaque As Integer
+            GameView.setarCoordenadasAtaque(linhaAtaque, colunaAtaque)
+            resultadoRodada = MapaController.fazerJogada(bot.getMapa, linhaAtaque, colunaAtaque)
+            GameView.imprimirTabuleiroInimigo(bot.getMapa)
+            If resultadoRodada = 2 Then
+                Console.WriteLine("Destruiu todas as embarcações inimigas! Você ganhou!")
+            ElseIf resultadoRodada = 1 Then
+                Console.WriteLine("Destruiu parte da embarcação! Continue assim!")
+            ElseIf resultadoRodada = 0 Then
+                Console.WriteLine("Acertou na agua!")
+            ElseIf resultadoRodada = -1 Then
+                Console.WriteLine("Você ja atacou aqui, tente em outro lugar.")
+            End If
+        End While
+
     End Function
 End Module
--- src/controllers/MapaController.vb
+++ src/controllers/MapaController.vb
@@ -5,18 +5,91 @@
         Return mapaCriado
     End Function
 
-    Public Function setarJogada(mapa As Mapa, posicaoX As Integer, posicaoY As Integer, tipoNavio As Integer)
-        If mapa.Item(posicaoX, posicaoY) <> 0 Then
+    Public Function setarJogada(mapa As Mapa, posicaoY As Integer, posicaoX As Integer, tipoNavio As Integer)
+        If mapa.Item(posicaoY, posicaoX) <> 0 Then
             '''se ja tiver marcado a posição no array, significa que ja houve jogada nessa posicao
             Return False
         Else
             '''1 = Submarino / 2 = Hiate ...
-            mapa.Item(posicaoX, posicaoY) = tipoNavio + 1
+            mapa.Item(posicaoY, posicaoX) = tipoNavio + 1
             Return True
         End If
     End Function
 
     Public Function setarEmbarcacao(mapa As Mapa, navio As Navio)
         Return mapa.setNavio(navio)
+    End Function
+
+    Public Function fazerJogada(mapa As Mapa, posicaoY As Integer, posicaoX As Integer)
+        If mapa.Item(posicaoY, posicaoX) <> 0 And mapa.Item(posicaoY, posicaoX) <> 6 Then
+            mapa.Item(posicaoY, posicaoX) = 6
+
+            If destruirNavio(mapa, posicaoY, posicaoX) = True Then
+                '''2 = acertou e acabou o jogo
+                Return 2
+            Else
+                '''1 = acertou mas nao acabou o jogo
+                Return 1
+            End If
+
+        ElseIf mapa.Item(posicaoY, posicaoX) = 0 Then
+            mapa.Item(posicaoY, posicaoX) = 6
+
+            '''0 = agua
+            Return 0
+        ElseIf mapa.Item(posicaoY, posicaoX) = 6 Then
+
+            '''-1 = refazer jogada
+            Return -1
+        End If
+    End Function
+
+    Public Function destruirNavio(mapa As Mapa, posicaoY As Integer, posicaoX As Integer)
+        Dim qntdNavios = mapa.getNaviosLength
+        Dim naviosDestruidos = 0
+
+        For i = 0 To qntdNavios
+            Dim navio As Navio = mapa.getNavio(i)
+            Dim qntdPosicoesNavio As Integer = navio.getQntdCasas
+
+            If checkNavioDestruido(navio, qntdPosicoesNavio) = False Then
+                For j = 0 To qntdPosicoesNavio - 1
+                    Dim coordenadasPosicao = navio.getPosicao(j)
+
+                    If coordenadasPosicao(0) = posicaoY And coordenadasPosicao(1) = posicaoX Then
+                        navio.setPosicao(-1, -1, j)
+                    End If
+
+                Next
+            End If
+
+            If checkNavioDestruido(navio, qntdPosicoesNavio) = True Then
+                naviosDestruidos += 1
+            End If
+        Next
+
+        If naviosDestruidos = (qntdNavios + 1) Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Public Function checkNavioDestruido(navio As Navio, qntdPosicoes As Integer)
+        Dim casasDestruidas = 0
+
+        For i = 0 To qntdPosicoes - 1
+            Dim coordenadasPosicao = navio.getPosicao(i)
+
+            If coordenadasPosicao(0) = -1 And coordenadasPosicao(1) = -1 Then
+                casasDestruidas += 1
+            End If
+        Next
+
+        If casasDestruidas = qntdPosicoes Then
+            Return True
+        Else
+            Return False
+        End If
     End Function
 End Module
